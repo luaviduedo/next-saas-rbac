022c87461c298e0bdd8a6bc54184235e26446d83
Lua Viduedo <luaviduedo@gmail.com>
2026-07-23 15:16:14 -0300

    create `seed.ts` to seed database

--- apps/api/src/lib/prisma.ts
+++ apps/api/src/lib/prisma.ts
@@ -2,7 +2,8 @@ import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 import 'dotenv/config'
 
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 export const prisma = new PrismaClient({
-  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+  adapter,
   log: ['query'],
 })
